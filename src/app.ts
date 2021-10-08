c262cf781111f41d93f74e23939d3f07a68c53dc
Khaled Badenjki <khaled.badenjki@swvl.com>
2021-10-09 01:50:30 +0200

    feat: add user model and service

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import logger from '@kottab/utils/logger'
 import {createServer} from '@kottab/utils/server'
+import db from '@kottab/utils/db'
+
+db.open()
+  .then(() => createServer())
+  .then(server => {
+    server.listen(3000, () => {
+      logger.info(`Listening on http://localhost:3000`)
+    })
+  })
+  .catch(err => {
+    logger.error(`Error: ${err}`)
+  })
 
 createServer()
   .then(server => {
