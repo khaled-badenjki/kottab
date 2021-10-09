47ffa95e4f29248659f464d8bb64e2b0ebc88bfe
Khaled Badenjki <khaled.badenjki@swvl.com>
2021-10-09 02:18:10 +0200

    feat: add users endpoints and tests for controller

--- src/api/controllers/__tests__/user_failure.ts
+++ src/api/controllers/__tests__/user_failure.ts
@@ -1,4 +1,5 @@
 import request from 'supertest'
+import faker from 'faker'
 import {Express} from 'express-serve-static-core'
 
 import UserService from '@kottab/api/services/user'
@@ -17,6 +18,25 @@ describe('auth failure', () => {
     request(server)
       .get(`/api/v1/goodbye`)
       .set('Authorization', 'Bearer fakeToken')
+      .expect(500)
+      .end(function(err, res) {
+        if (err) return done(err)
+        expect(res.body).toMatchObject({error: {type: 'internal_server_error', message: 'Internal Server Error'}})
+        done()
+      })
+  })
+})
+
+describe('createUser failure', () => {
+  it('should return 500 & valid response if auth rejects with an error', done => {
+    (UserService.createUser as jest.Mock).mockResolvedValue({error: {type: 'unkonwn'}})
+    request(server)
+      .post(`/api/v1/user`)
+      .send({
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        name: faker.name.firstName()
+      })
       .expect(500)
       .end(function(err, res) {
         if (err) return done(err)
--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 
 import UserService from '@kottab/api/services/user'
 import {writeJsonResponse} from '@kottab/utils/express'
+import logger from '@kottab/utils/logger'
 
 export function auth(req: express.Request, res: express.Response, next: express.NextFunction): void {
   const token = req.headers.authorization!
@@ -17,6 +18,27 @@ export function auth(req: express.Request, res: express.Response, next: express.
       }
     })
     .catch(err => {
+      writeJsonResponse(res, 500, {error: {type: 'internal_server_error', message: 'Internal Server Error'}})
+    })
+}
+
+export function createUser(req: express.Request, res: express.Response, next: express.NextFunction): void {
+  const {email, password, name} = req.body
+
+  UserService.createUser(email, password, name)
+    .then(resp => {
+      if ((resp as any).error) {
+        if ((resp as any).error.type === 'account_already_exists') {
+          writeJsonResponse(res, 409, resp)
+        } else {
+          throw new Error(`unsupported ${resp}`)
+        }
+      } else {
+        writeJsonResponse(res, 201, resp)
+      }
+    })
+    .catch((err: any) => {
+      logger.error(`createUser: ${err}`)
       writeJsonResponse(res, 500, {error: {type: 'internal_server_error', message: 'Internal Server Error'}})
     })
 }
