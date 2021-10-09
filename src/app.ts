25c42be2295133ff1ecd653a4d33f024d8633d2d
Khaled Badenjki <khaled.badenjki@swvl.com>
2021-10-09 02:36:18 +0200

    feat: connect kottab to mongodb docker

--- src/app.ts
+++ src/app.ts
@@ -13,12 +13,3 @@ db.open()
     logger.error(`Error: ${err}`)
   })
 
-createServer()
-  .then(server => {
-    server.listen(3000, () => {
-      logger.info(`Listening on http://localhost:3000`)
-    })
-  })
-  .catch(err => {
-    logger.error(`Error: ${err}`)
-  })
